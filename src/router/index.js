ef7cb830ea678672184867e821f2eae0200b1620
Carlos Fabian Camilo Caicedo <camilocaicedocarlosfabian40@gmail.com>
2024-05-17 21:14:11 -0500

    Index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,21 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+
+import EditarEquipo from '../components/Equipo/EditarEquipo.vue';
+import NewEquipo from '../components/Equipo/NewEquipo.vue';
+import Equipos from '../views/Equipos.vue';
+import EditarReserva from '../components/Reserva/EditarReserva.vue';
+import NewReserva from '../components/Reserva/NewReserva.vue';
+import Reservas from '../views/Reservas.vue';
+import EditarCliente from '../components/Cliente/EditarCliente.vue';
+import NewCliente from '../components/Cliente/NewCliente.vue';
+import Clientes from '../views/Clientes.vue';
+import EditarContrato from '../components/Contrato/EditarContrato.vue';
+import NewContrato from '../components/Contrato/NewContrato.vue';
+import Contratos from '../views/Contratos.vue';
+import EditarPago from '../components/Pago/EditarPago.vue';
+import NewPago from '../components/Pago/NewPago.vue';
+import Pagos from '../views/Pagos.vue';
+import HomeView from '../views/HomeView.vue';
 
 const routes = [
   {
@@ -8,14 +24,81 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/equipos',
+    name: 'equipos',
+    component: Equipos
+  },
+  {
+    path: '/editar-equipo/:id',
+    name: 'EditarEquipo',
+    component: EditarEquipo
+  },
+  {
+    path: '/add-equipo/',
+    name: 'NewEquipo',
+    component: NewEquipo
+  },
+  {
+    path: '/reservas',
+    name: 'reservas',
+    component: Reservas
+  },
+  {
+    path: '/editar-reserva/:id',
+    name: 'EditarReserva',
+    component: EditarReserva
+  },
+  {
+    path: '/add-reserva/',
+    name: 'NewReserva',
+    component: NewReserva
+  },
+  {
+    path: '/clientes',
+    name: 'clientes',
+    component: Clientes
+  },
+  {
+    path: '/editar-cliente/:id',
+    name: 'EditarCliente',
+    component: EditarCliente
+  },
+  {
+    path: '/add-cliente/',
+    name: 'NewCliente',
+    component: NewCliente
+  },
+  {
+    path: '/contratos',
+    name: 'contratos',
+    component: Contratos
+  },
+  {
+    path: '/editar-contrato/:id',
+    name: 'EditarContrato',
+    component: EditarContrato
+  },
+  {
+    path: '/add-contrato/',
+    name: 'NewContrato',
+    component: NewContrato
+  },
+  {
+    path: '/pagos',
+    name: 'pagos',
+    component: Pagos
+  },
+  {
+    path: '/editar-pago/:id',
+    name: 'EditarPago',
+    component: EditarPago
+  },
+  {
+    path: '/add-pago/',
+    name: 'NewPago',
+    component: NewPago
   }
-]
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
