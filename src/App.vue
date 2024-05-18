98c8958946f8c3ee79ca8ea27492440f89d33d00
Carlos Fabian Camilo Caicedo <camilocaicedocarlosfabian40@gmail.com>
2024-05-17 20:48:10 -0500

    Rutas en el navar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div id="app">
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link to="/clientes">Clientes</router-link> |
+      <router-link to="/equipos">Equipos</router-link> |
+      <router-link to="/reservas">Reservas</router-link> |
+      <router-link to="/contratos">Contratos</router-link> |
+      <router-link to="/pagos">Pagos</router-link>
+    </nav>
+    <router-view/>
+  </div>
 </template>
 
 <style>
